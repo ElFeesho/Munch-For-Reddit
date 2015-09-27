d3b98765fb658821bef39bb79b1c60d85bd5d36d
Chris Sawczuk <gummybassist@gmail.com>
2015-09-27 19:15:52 +0100

    Refactored CommentAdapter. Simplified logic for formatting comment row

--- app/src/main/java/com/example/naren/munch/adapters/CommentAdapter.java
+++ app/src/main/java/com/example/naren/munch/adapters/CommentAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.support.v4.content.ContextCompat;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -18,117 +17,61 @@ import java.util.ArrayList;
 
 public class CommentAdapter extends ArrayAdapter<Comment> {
 
-    ArrayList<Comment> comments = new ArrayList<>();
+    private static class CommentRowViewHolder {
+
+        View comment_indicator_space;
+        LinearLayout comment_indicator_color, comment_container;
+        TextView comment_author, comment_score, comment_time, comment_body;
+
+    }
+
+    private final static int[] commentIndicatorColours = {R.color.blue_800,
+            R.color.purple_800,
+            R.color.green_800,
+            R.color.orange_800,
+            R.color.red_800,
+            R.color.yellow_800
+    };
 
     public CommentAdapter(Context context, ArrayList<Comment> comments) {
         super(context, 0, comments);
-        this.comments = comments;
-
+        this.addAll(comments);
     }
-
-    @Override
-    public int getCount() {
-        return comments.size();
-    }
-
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        MyViewHolder holder;
+        CommentRowViewHolder holder;
 
         Comment comment = getItem(position);
 
-        //Figure out how to properly display post time
-
-//        final long time = comment.getComment_time();
-//        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-//        calendar.setTimeZone(TimeZone.getDefault());
-//        calendar.setTimeInMillis(time * 1000);
-//
-//
-//        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
-//        int minute = calendar.get(Calendar.MINUTE);
-
         if (convertView == null) {
 
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.comment_row, parent, false);
-            holder = new MyViewHolder();
+            convertView = View.inflate(getContext(), R.layout.comment_row, null);
+            holder = new CommentRowViewHolder();
 
-            holder.comment_indicator_space = (LinearLayout) convertView.findViewById(R.id.comment_indicator_space);
+            holder.comment_indicator_space = convertView.findViewById(R.id.comment_indicator_space);
             holder.comment_indicator_color = (LinearLayout) convertView.findViewById(R.id.comment_indicator_color);
             holder.comment_author = (TextView) convertView.findViewById(R.id.comment_author);
             holder.comment_score = (TextView) convertView.findViewById(R.id.comment_score);
-//            holder.comment_time = (TextView) convertView.findViewById(R.id.comment_time);
             holder.comment_body = (TextView) convertView.findViewById(R.id.comment_body);
 
             convertView.setTag(holder);
-
-
         } else {
-
-            holder = (MyViewHolder) convertView.getTag();
+            holder = (CommentRowViewHolder) convertView.getTag();
         }
         holder.comment_author.setText(comment.getComment_author());
-        holder.comment_score.setText(comment.getComment_score() + "points");
+        holder.comment_score.setText(parent.getContext().getString(R.string.comment_karma, comment.getComment_score()));
         holder.comment_body.setText(Html.fromHtml(comment.getComment_body()));
         holder.comment_body.setMovementMethod(LinkMovementMethod.getInstance());
-//        holder.comment_time.setText(hour + " hrs ago");
 
-
-        //handle comment replies/children and set the appropriate indication colors
-        if (comments.get(position).level == 0) {
-
-            holder.comment_indicator_space.setPadding(0, 0, 0, 0);
-            holder.comment_indicator_color.setVisibility(View.GONE);
-
-        } else if (comments.get(position).level == 1) {
-
-            holder.comment_indicator_space.setPadding(comments.get(position).level * 8, 0, 0, 0);
-            holder.comment_indicator_color.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.blue_800));
-            holder.comment_indicator_color.setVisibility(View.VISIBLE);
-
-        } else if (comments.get(position).level == 2) {
-
-            holder.comment_indicator_space.setPadding(comments.get(position).level * 8, 0, 0, 0);
-            holder.comment_indicator_color.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.purple_800));
-            holder.comment_indicator_color.setVisibility(View.VISIBLE);
-
-        } else if (comments.get(position).level == 3) {
-
-
-            holder.comment_indicator_space.setPadding(comments.get(position).level * 8, 0, 0, 0);
-            holder.comment_indicator_color.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.green_800));
-            holder.comment_indicator_color.setVisibility(View.VISIBLE);
-
-        } else if (comments.get(position).level == 4) {
-
-            holder.comment_indicator_space.setPadding(comments.get(position).level * 8, 0, 0, 0);
-            holder.comment_indicator_color.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.orange_800));
-            holder.comment_indicator_color.setVisibility(View.VISIBLE);
-
-        } else if (comments.get(position).level == 5) {
-
-            holder.comment_indicator_space.setPadding(comments.get(position).level * 8, 0, 0, 0);
-            holder.comment_indicator_color.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.red_800));
-            holder.comment_indicator_color.setVisibility(View.VISIBLE);
-
-        } else if (comments.get(position).level == 6) {
-
-            holder.comment_indicator_space.setPadding(comments.get(position).level * 8, 0, 0, 0);
-            holder.comment_indicator_color.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.yellow_800));
-
-            holder.comment_indicator_color.setVisibility(View.VISIBLE);
-        }
+        ViewGroup.LayoutParams layoutParams = holder.comment_indicator_space.getLayoutParams();
+        layoutParams.width = comment.level * 8;
+        holder.comment_indicator_color.setBackgroundColor(ContextCompat.getColor(getContext(), commentIndicatorColours[comment.level % commentIndicatorColours.length]));
+        holder.comment_indicator_color.setVisibility(comment.level == 0 ? View.GONE : View.VISIBLE);
+        holder.comment_indicator_space.setLayoutParams(layoutParams);
 
         return convertView;
-    }
-
-    private static class MyViewHolder {
-
-        LinearLayout comment_indicator_space, comment_indicator_color, comment_container;
-        TextView comment_author, comment_score, comment_time, comment_body;
-
     }
 
 }
