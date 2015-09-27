13aca0e63aa9ce534363077738ef0116a569be2b
Chris Sawczuk <gummybassist@gmail.com>
2015-09-27 17:00:06 +0100

    Removed superfluous method override and comments

--- app/src/main/java/com/example/naren/munch/MainActivity.java
+++ app/src/main/java/com/example/naren/munch/MainActivity.java
@@ -119,7 +119,6 @@ public class MainActivity extends AppCompatActivity {
         mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
-
                 mViewPager.setCurrentItem(mAdapter.positionForSubreddit(menuItem.getItemId()), true);
                 return true;
             }
@@ -128,25 +127,12 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onBackPressed() {
-
         if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
             mDrawerLayout.closeDrawer(GravityCompat.START);
         } else {
