21112e2d94105cfe2aabe6a9c8e1127c8ac767cc
Chris Sawczuk <gummybassist@gmail.com>
2015-09-27 16:58:06 +0100

    Centralised subreddit id to position knowledge in subreddit adapter

--- app/src/main/java/com/example/naren/munch/MainActivity.java
+++ app/src/main/java/com/example/naren/munch/MainActivity.java
@@ -21,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
     public static class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
+        private final static int[] subredditIds = {
+                R.id.item_frontpage, R.id.item_all, R.id.item_aww,
+                R.id.item_art, R.id.item_askreddit, R.id.item_askscience, R.id.item_announcements,
+                R.id.item_blog, R.id.item_books, R.id.item_creepy, R.id.item_dataisbeautiful,
+                R.id.item_diy, R.id.item_documentaries, R.id.item_earthporn, R.id.item_eli5,
+                R.id.item_fitness, R.id.item_food, R.id.item_funny, R.id.item_futurology,
+                R.id.item_gadgets, R.id.item_gaming, R.id.item_getmotivated, R.id.item_gifs,
+                R.id.item_history, R.id.item_iama, R.id.item_internetisbeautiful, R.id.item_jokes, R.id.item_lpt,
+                R.id.item_listentothis, R.id.item_mildlyinteresting, R.id.item_movies, R.id.item_music,
+                R.id.item_news, R.id.item_nosleep, R.id.item_nottheonion, R.id.item_oldschoolcool,
+                R.id.item_personalfinance, R.id.item_philosophy, R.id.item_photoshopbattles, R.id.item_pics,
+                R.id.item_science, R.id.item_showerthoughts, R.id.item_space, R.id.item_sports,
+                R.id.item_television, R.id.item_tifu, R.id.item_til, R.id.item_twox,
+                R.id.item_upnews, R.id.item_videos, R.id.item_worldnews, R.id.item_writingprompts
+        };
+
         private final static String[] FEED_TITLES =
 
                 {"frontpage", "all", "aww", "art", "askreddit", "askscience",
@@ -51,6 +67,23 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return FEED_TITLES[position];
         }
+
+        private int lookupSubredditIdPosition(int itemId) {
+            int position = 0;
+            for (int i = 0; i < subredditIds.length; i++)
+            {
+                if (subredditIds[i] == itemId)
+                {
+                    position = i;
+                    break;
+                }
+            }
+            return position;
+        }
+
+        public int positionForSubreddit(int itemId) {
+            return lookupSubredditIdPosition(itemId);
+        }
     }
 
     private DrawerLayout mDrawerLayout;
@@ -60,23 +93,6 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
     private ViewPagerAdapter mAdapter;
-
-    private final static int[] subredditIds = {
-            R.id.item_frontpage, R.id.item_all, R.id.item_aww,
-            R.id.item_art, R.id.item_askreddit, R.id.item_askscience, R.id.item_announcements,
-            R.id.item_blog, R.id.item_books, R.id.item_creepy, R.id.item_dataisbeautiful,
-            R.id.item_diy, R.id.item_documentaries, R.id.item_earthporn, R.id.item_eli5,
-            R.id.item_fitness, R.id.item_food, R.id.item_funny, R.id.item_futurology,
-            R.id.item_gadgets, R.id.item_gaming, R.id.item_getmotivated, R.id.item_gifs,
-            R.id.item_history, R.id.item_iama, R.id.item_internetisbeautiful, R.id.item_jokes, R.id.item_lpt,
-            R.id.item_listentothis, R.id.item_mildlyinteresting, R.id.item_movies, R.id.item_music,
-            R.id.item_news, R.id.item_nosleep, R.id.item_nottheonion, R.id.item_oldschoolcool,
-            R.id.item_personalfinance, R.id.item_philosophy, R.id.item_photoshopbattles, R.id.item_pics,
-            R.id.item_science, R.id.item_showerthoughts, R.id.item_space, R.id.item_sports,
-            R.id.item_television, R.id.item_tifu, R.id.item_til, R.id.item_twox,
-            R.id.item_upnews, R.id.item_videos, R.id.item_worldnews, R.id.item_writingprompts
-    };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,26 +120,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
 
-                mViewPager.setCurrentItem(lookupSubredditIdPosition(menuItem.getItemId()), true);
+                mViewPager.setCurrentItem(mAdapter.positionForSubreddit(menuItem.getItemId()), true);
                 return true;
             }
         });
 
     }
-
-    private int lookupSubredditIdPosition(int itemId) {
-        int position = 0;
-        for (int i = 0; i < subredditIds.length; i++)
-        {
-            if (subredditIds[i] == itemId)
-            {
-                position = i;
-                break;
-            }
-        }
-        return position;
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
